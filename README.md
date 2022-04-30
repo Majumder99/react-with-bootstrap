d893f86e239c0906988b0ddf9dcf0d3491615d78
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-04-30 20:27:34 +0600

    new

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### This is mainly to integrate bootstrap with react.js
 
 $ npm i bootstrap // To install the bootstrap
+$ npm i @fortawesome/react-fontawesome // To install the fontawesome
+$ npm i react-typed // To install the typed animation
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-secondary">Welcome bootstrap</h1>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
